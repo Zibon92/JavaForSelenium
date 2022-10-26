adc7a3394e605eaf5e351f2dc5605a7e733e1e79
Zibon Chowdhury <55887424+Zibon1992@users.noreply.github.com>
2022-10-26 17:28:33 +0600

    JavaForSelenium First Commit

---
+++ src/conditionalStatement/ifStatementLearning.java
@@ -0,0 +1,16 @@
+package conditionalStatement;
+
+public class ifStatementLearning {
+
+	public static void main(String[] args) {
+		System.out.println("test start");
+		
+		int i=50;
+		if(i<60)
+		{
+			System.out.println("Value: "+i);
+		}
+		System.out.println("test end");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ebadul-PC
+ *
+ */
+module javalearning {
+}
