bd9b3427d18bf8f616d8a6147e9c4149bf1207dd
Zibon Chowdhury <55887424+Zibon1992@users.noreply.github.com>
2022-11-03 20:22:16 +0600

    Inheritenc file added

---
+++ src/learnInheritence/BaseClass.java
@@ -0,0 +1,14 @@
+package learnInheritence;
+
+public class BaseClass
+{
+	public void add()
+	{
+		System.out.println("I am base class and I am addition");
+	}
+	public void sub()
+	{
+		System.out.println("I am base class and I am subtraction");
+	}
+
+}
---
+++ src/learnInheritence/ChildClass.java
@@ -0,0 +1,20 @@
+package learnInheritence;
+
+public class ChildClass {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+	}
+	public void mul()
+	{
+		System.out.println("I am base class and I am multiplication");
+	}
+	public void div()
+	{
+		System.out.println("I am base class and I am division");
+	}
+
+}
